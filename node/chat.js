29491e1bb9ba98eb941453d542e55ca2be71ecb4
Rafael <torresrafael@gmail.com>
2012-03-06 23:00:30 -0600

    console.write

--- node/chat.js
+++ node/chat.js
@@ -3,10 +3,10 @@ var net = require('net');
 var chatServer = net.createServer();
 
 chatServer.on('connection', function(client){
-	client.write('hello world!\n');
-	client.write('bye!\n');
-	
-	client.end();
+	client.write('hi!\n');
+	client.on('data', function(data){
+		console.log(data);		
+	});	
 });
 
 chatServer.listen(9000);
