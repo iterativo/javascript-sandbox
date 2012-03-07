969ee9fc1c908e0cbd4e0ee462f91552500eed6f
Rafael <torresrafael@gmail.com>
2012-03-06 23:12:13 -0600

    multiple client listeners

--- node/chat.js
+++ node/chat.js
@@ -2,10 +2,15 @@ var net = require('net');
 
 var chatServer = net.createServer();
 
+clientList = [];
+
 chatServer.on('connection', function(client){
 	client.write('hi!\n');
+	clientList.push(client);
 	client.on('data', function(data){
-		console.log(data);		
+		for(var i = 0; i < clientList.length; i+=1){
+			clientList[i].write(data);		
+		}
 	});	
 });
 
